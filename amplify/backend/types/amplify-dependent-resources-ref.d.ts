7b3f403252b5befe61721ccde91b58b8e6c19970
lightatone <sfeng023@ucr.edu>
2024-03-19 17:31:36 -0700

    Update code, now can email. upload feature still have issure to login

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,12 +6,29 @@ export type AmplifyDependentResourcesAttributes = {
       "RootUrl": "string"
     }
   },
+  "auth": {
+    "itsupport2274d5ee": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "itsupportd7f7362d": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "itsupport": {
+      "BucketName": "string",
       "Region": "string"
     }
   }
--- src/TicketForm.js
+++ src/TicketForm.js
@@ -3,48 +3,59 @@ import { Form, Input, Button, Upload, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import './TicketForm.css';
 import axios from 'axios'; 
+import { uploadData  } from 'aws-amplify/storage';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 const TicketForm = ({ onSubmit }) => {
   const [form] = Form.useForm();
   
   const onFinish = async (values) => {
-  
-    console.log("here are the values", values);
-    // 使用axios发送POST请求到你的Lambda函数的API终端
-    // try {
-    //   const response = await axios.post('https://vca5r6zcoc.execute-api.us-east-2.amazonaws.com/staging/sumbit', values);
-    //   console.log('Success:', response.data);
-    //   message.success('Ticket submitted successfully!');
-    // } catch (error) {
-    //   console.error('Failed to submit ticket:', error);
-    //   message.error('Failed to submit the ticket. Please try again.');
-    // }
-     // 构建请求体，确保它包含发送邮件所需的字段
-     const emailData = {
-      to: 'it@itsupportdesks.com', // 收件人地址
-      from: 'it@itsupportdesks.com', // 发件人地址，必须是在SES中验证过的
-      subject: 'Test Email from SES', // 邮件主题
-      body: 'Hello, this is a test email sent from AWS SES via Lambda.' // 邮件正文
-    };
-
-    try {
-      const response = await axios.post('https://vca5r6zcoc.execute-api.us-east-2.amazonaws.com/staging/sumbit', emailData);
-      console.log('Success:', response.data);
-      // 根据需要添加成功消息
-    } catch (error) {
-      console.error('Failed to send email:', error);
-      // 根据需要添加失败消息
+    let formData = { ...values };
+    if (values.attachment && values.attachment.length > 0) {
+      console.log("result 22");
+      const uploadPromises = values.attachment.map(file =>
+        file.originFileObj ? handleUpload(file.originFileObj) : Promise.resolve('')
+      );
+      const fileUrls = await Promise.all(uploadPromises);
+      console.log("Here is ", fileUrls);
+      // 过滤掉空字符串，只保留成功上传的文件URL
+      formData.attachment = fileUrls.filter(url => url !== '');
     }
+    console.log("result 22", formData);
+    const now = new Date();
+    const ticketSupportID = `Ticket Support ID: ${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}`;
+    
+    const emailData = {
+      to: 'it@itsupportdesks.com', // 收件人地址
+      from: 'it@itsupportdesks.com', // 发件人地址，必须是在SES中验证过的.沙盒模式。
+      subject: `Ticket Support - ${ticketSupportID}`, // 邮件主题
+      body: `Hello, Ticket Support 
+
+      Email: ${values.email}
+      Remote PC ID: ${values.remotePCID}
+      Phone Extension: ${values.phoneExtension}
+      Description: ${values.description}
+      附件URLs: ${formData.attachment.join(', ')}
+
+      Best regards,
+      Your IT Support`
+    };
+    console.log("Here is the email data", emailData)
+    // try {
+    //   const response = await axios.post('https://vca5r6zcoc.execute-api.us-east-2.amazonaws.com/staging/sumbit', emailData);
+    //   console.log('Success:', response.data);
+    //   // 根据需要添加成功消息
+    // } catch (error) {
+    //   console.error('Failed to send email:', error);
+    //   // 根据需要添加失败消息
+    // }
     
   };
 
-  const normFile = (e) => {
-    if (Array.isArray(e)) {
-      return e;
-    }
-    return e?.fileList;
-  };
-
+  /*********************************Attchement check and upload handle*********************************************** */
+  
   const beforeUpload = (file) => {
     const isDocOrPdf = file.type === 'application/pdf' || file.type === 'application/msword' || file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
     if (!isDocOrPdf) {
@@ -56,6 +67,48 @@ const TicketForm = ({ onSubmit }) => {
     }
     return isLessThan5M && isDocOrPdf || Upload.LIST_IGNORE; // 返回false或Upload.LIST_IGNORE将停止上传，但Upload.LIST_IGNORE不会显示错误信息
   };
+
+  const handleUpload = async (file) => {
+    console.log("here handle upload");
+    try {
+      const result = await uploadData({
+        key: file.name,
+        data: file,
+        options: {
+          accessLevel: 'guest', // defaults to `guest` but can be 'private' | 'protected' | 'guest'
+        }
+      }).result;
+      
+      console.log('Succeeded: ', result);
+      return result.url;
+    } catch (error) {
+      console.log('Error uploading file: ', error);
+      return '';
+    }
+  };
+
+  const normFile =  (e) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+
+    return e?.fileList || [];
+  
+    // // 确保fileList是一个数组。如果e.fileList不存在或不是数组，使用空数组作为默认值
+    // const fileList = Array.isArray(e?.fileList) ? e.fileList : [];
+    // console.log("hello here is norma File");
+    // // 使用.map和Promise.all处理文件上传，并等待所有上传操作完成
+    // const uploadPromises = fileList.map(file =>
+    //   file.originFileObj ? handleUpload(file.originFileObj) : Promise.resolve('')
+    // );
+    // const fileUrls = await Promise.all(uploadPromises);
+  
+    // // 过滤掉空字符串，只返回成功上传的文件URL
+    // return fileUrls.filter(url => url !== '');
+  };
+  
+  /******************************************************************************** */
 
   return (
     <Form form={form} onFinish={onFinish} layout="vertical" name="ticketForm">
